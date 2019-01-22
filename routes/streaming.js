17dce964e817795f47e4fccb82db2b3223859ad3
hwiesung <hwiesung@gmail.com>
2019-01-22 18:03:54 +0900

    s3 video file streaming

---
+++ routes/streaming.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var logger = require('../common/logger');
+/* GET users listing. */
+var config = require('config');
+
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({
+    accessKeyId: config.awsKey.access_key_id,
+    secretAccessKey: config.awsKey.secret_access_key
+});
+
+router.get('/:bucket/:key', function(req, res) {
+    let bucket = req.params.bucket;
+    let key = req.params.key;
+    logger.info(bucket+'/'+key);
+    const params = {
+        Bucket: bucket,
+        Key: key
+    };
+
+    s3.getObject(params)
+        .on('httpHeaders', function (statusCode, headers) {
+            console.log(headers);
+            res.set('Content-Length', headers['content-length']);
+            res.set('Content-Type', headers['content-type']);
+            this.response.httpResponse.createUnbufferedStream()
+                .pipe(res);
+        })
+        .send();
+});
+
+module.exports = router;
